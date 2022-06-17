986c86601cea5bc57bbdd2bc0f65e297d9492c2c
Akash Jaiswal <akashjaiswal797112@gmail.com>
2022-06-17 15:03:33 +0530

    Time: 0 ms (100.00%), Space: 43 MB (28.27%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -3,22 +3,14 @@ class Solution {
         
         int r=0;
         int c=matrix[r].length-1;
-        while(r < matrix.length && c >= 0)
-        {
+        while(r < matrix.length && c >= 0){
             if(matrix[r][c] == target)
-            {
                 return true;
-            }
             if(matrix[r][c] < target)
-            {
                 r++;
-            }
             else
-            {
                 c--;
-            }
         }
-        
         return false;
         
     }
