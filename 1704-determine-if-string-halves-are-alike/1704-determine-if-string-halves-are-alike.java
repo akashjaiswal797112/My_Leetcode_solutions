59da61973b769ce2cba2bcbbf76c1fec62f55629
Akash Jaiswal <akashjaiswal797112@gmail.com>
2022-07-22 19:30:42 +0530

    Time: 7 ms (45.42%), Space: 42.6 MB (32.93%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,36 +3,108 @@ class Solution {
         
        
 
+        StringBuilder builder1 = new StringBuilder();
+        StringBuilder builder2 = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if(i>=s.length()/2)
+            {
+                builder2.append(s.charAt(i));
+            }
+            else
+            {
+                builder1.append(s.charAt(i));
+            }
+
+        }
 
          int count1 = 0;
          int count2 = 0;
 
 
 
- for (int a = 0; a < s.length()/2; a++) {
-
-            if(s.charAt(a)=='a' || s.charAt(a)=='e' || s.charAt(a)=='i' || s.charAt(a)=='o' || s.charAt(a)=='u' ||
-                    s.charAt(a)=='A' || s.charAt(a)=='E' || s.charAt(a)=='I' || s.charAt(a)=='O' || s.charAt(a)=='U')
+        for (int a = 0; a < builder1.length(); a++) {
+            switch (builder1.charAt(a))
             {
-                count1++;
-            }
+                case 'a':
+                    count1++;
+                    break;
 
+                case 'e':
+                    count1++;
+                    break;
+
+                case 'i':
+                    count1++;
+                    break;
+
+                case 'o':
+                    count1++;
+                    break;
+
+                case 'u':
+                    count1++;
+                    break;
+
+                case 'A':
+                    count1++;
+                    break;
+
+                case 'E':
+                    count1++;
+                    break;
+
+                case 'I':
+                    count1++;
+                    break;
+
+                case 'O':
+                    count1++;
+                    break;
+
+                case 'U':
+                    count1++;
+                    break;
+            }
         }
 
-
-
-            for (int b = s.length()/2; b < s.length(); b++) {
-                if(s.charAt(b)=='a' || s.charAt(b)=='e' || s.charAt(b)=='i' || s.charAt(b)=='o' || s.charAt(b)=='u' ||
-                        s.charAt(b)=='A' || s.charAt(b)=='E' || s.charAt(b)=='I' || s.charAt(b)=='O' || s.charAt(b)=='U')
+            for (int b = 0; b < builder2.length(); b++) {
+                switch(builder2.charAt(b))
                 {
-                    count2++;
+                    case 'a' : count2++;
+                        break;
+
+                    case 'e' : count2++;
+                        break;
+
+                    case 'i' : count2++;
+                        break;
+
+                    case 'o' : count2++;
+                        break;
+
+                    case 'u' : count2++;
+                        break;
+
+                    case 'A' : count2++;
+                        break;
+
+                    case 'E' : count2++;
+                        break;
+
+                    case 'I' : count2++;
+                        break;
+
+                    case 'O' : count2++;
+                        break;
+
+                    case 'U' : count2++;
+                        break;
                 }
-        }       
+        }
 
-
-
-
-
+      
         if(count1==count2)
         {
             return true;
