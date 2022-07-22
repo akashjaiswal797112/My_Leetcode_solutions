0b0c3030ef6213764ae31be82b3473f3fccdb3f9
Akash Jaiswal <akashjaiswal797112@gmail.com>
2022-07-22 18:35:35 +0530

    Time: 1 ms (90.11%), Space: 40.4 MB (91.17%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,11 +1,9 @@
 class Solution {
     public String interpret(String command) {
         
-       String a = command.replace("()","o");
-       String last =  a.replace("(al)","al");
-
-        
-        return last;
+       String a = command.replace("()","o").replace("(al)","al");
+     //  String last =  a.replace("(al)","al");
+        return a;
         
         
 // return command.replace("()","o").replace("(al)","al");
