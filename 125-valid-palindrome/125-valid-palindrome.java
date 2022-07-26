672bdd515cdd317e81205745dd4c3a2a6219d52d
Akash Jaiswal <akashjaiswal797112@gmail.com>
2022-07-26 10:34:30 +0530

    Time: 17 ms (42.95%), Space: 43.3 MB (80.36%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,19 +1,49 @@
-class Solution {
-    public boolean isPalindrome(String s) {
+// class Solution {
+//     public boolean isPalindrome(String s) {
         
        
-        StringBuilder builder = new StringBuilder();
-        String str=s.toLowerCase();
-        for (int i = 0; i < str.length(); i++){
+//         StringBuilder builder = new StringBuilder();
+//         String str=s.toLowerCase();
+//         for (int i = 0; i < str.length(); i++){
          
-            if(  ((str.charAt(i)>='a') && (str.charAt(i)<='z')) || ((str.charAt(i)>='0') && (str.charAt(i)<='9')))
-            builder.append(str.charAt(i));
-        }
-        String oldString = builder.toString();
-        String reverse = String.valueOf(builder.reverse());
-        if(oldString.equals(reverse))
-            return true;
-        else
-            return false;
-    }
+//             if(  ((str.charAt(i)>='a') && (str.charAt(i)<='z')) || ((str.charAt(i)>='0') && (str.charAt(i)<='9')))
+//             builder.append(str.charAt(i));
+//         }
+//         String oldString = builder.toString();
+//         String reverse = String.valueOf(builder.reverse());
+//         if(oldString.equals(reverse))
+//             return true;
+//         else
+//             return false;
+//     }
+// }
+
+class Solution {
+public boolean isPalindrome(String s) {
+
+    // base case
+    if(s.length() < 2) return true;
+    
+    // convert given string to lowercase
+    String s1 = s.toLowerCase();
+    
+    // keep only alphanumeric characters
+    String newStr = s1.replaceAll("[^a-z0-9]+", "");
+    
+    // create new string by reversing newStr 
+    String check = reverse(newStr);
+    
+    // if the two strings match, then it is a palindrome
+    if(newStr.equals(check))
+        return true;
+    
+    return false;
+}
+
+// function to reverse a given string
+private String reverse(String str) {
+    StringBuilder res = new StringBuilder(str);
+    res.reverse();
+    return res.toString();
+}
 }
