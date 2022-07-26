9382b5db0b6a8b54457f361231a90d118f089c77
Akash Jaiswal <akashjaiswal797112@gmail.com>
2022-07-26 21:29:50 +0530

    Time: 1 ms (91.33%), Space: 40.4 MB (97.41%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -3,20 +3,14 @@ class Solution {
         
          int count=0;
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-
+        for (int i = 0; i < s.length(); i++) 
+        {
             if(s.charAt(i)==' ')
-            {
                 count++;
-            }
             if(count==k)
-            {
-                break;
-            }
-                else {
+                break;   
+            else 
                 builder.append(s.charAt(i));
-            }
-
         }
        return builder.toString();        
     }
